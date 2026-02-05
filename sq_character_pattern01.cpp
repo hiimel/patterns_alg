b4713ded90fecae1c28f524ed8ece580fd28feb7
hiimel <himel.browser@gmail.com>
2026-02-05 12:02:52 +0600

    diff types of patterns

---
+++ sq_character_pattern01.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    char ch = 'A';
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n; j++) {
+            cout << "A" << " ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
